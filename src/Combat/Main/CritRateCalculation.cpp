47c517949013efa04a64e73d187011f7569a0930
Paul Orzel <pb.orzel@proton.me>
2026-07-26 14:56:39 +0200

    CalculateCritRate cleanup

--- src/Combat/Main/CritRateCalculation.cpp
+++ src/Combat/Main/CritRateCalculation.cpp
@@ -1,19 +1,19 @@
 #include <globaldefs.h>
 ARM float CalculateCritRate(int deftness, float accessoryBonus, float bookBonus, float skillBonus, unsigned char hitCount) {
-    float baseChanceFloat;
-    float critChance;
-    float hitCountFloat;
     float minimumChance = 2.0f;
-    int baseChance = (deftness - 150) * 0x10000 >> 0x10;
+
+    // there is some bit shifting in the assembly here, 16 left and 16 right and those preserve the most significant bit. A conversion to short seems extremely likely
+    short baseChance = (deftness - 150);
     if (baseChance < 0) {
         baseChance = 0;
     }
-    baseChanceFloat = baseChance;
-    baseChanceFloat = 0.01f * baseChanceFloat;
-    hitCountFloat = hitCount;
-    hitCountFloat = 1.0f / hitCountFloat; // results in multiplying by 0.5 for 2 hits, 0.333~ for 3, etc. kinda makes crits on multihit moves bad though
+    float baseChanceFloat = 0.01f * (float)baseChance;    
+    
+    float hitCountFloat = 1.0f / float(hitCount); // results in multiplying by 0.5 for 2 hits, 0.333~ for 3, etc. kinda makes crits on multihit moves bad though
+    
     baseChanceFloat = minimumChance + baseChanceFloat;
-    critChance = accessoryBonus + baseChanceFloat;
+
+    float critChance = accessoryBonus + baseChanceFloat;
     critChance = bookBonus + critChance;
     critChance = skillBonus * critChance;
     return hitCountFloat * critChance;
